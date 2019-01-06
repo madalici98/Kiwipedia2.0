648a48be9ef3d7082474ee58e5ab94d037546564
Andreyutul3000 <34367689+Andreyutul3000@users.noreply.github.com>
2019-01-06 14:07:56 +0200

    Categorii, sortari, search
    Am introdus categoriile, am facut sortari pe categorii. Am introdus filtre pentru vechime si ordine alfabetica. Am adaugat un search engine banal dar functional. Mici schimbari UI

--- Kiwipedia2.0/Models/Article.cs
+++ Kiwipedia2.0/Models/Article.cs
@@ -11,6 +11,7 @@ namespace Kiwipedia2._0.Models
         public ArticleVersion currentVersionId { get; set; }  //deocamdata fac legatura directa iar terminam baza de date o sa fac cu id
         public DateTime creationDate { get; set; }
         public string creatorId { get; set; }
+        public string category { get; set; }
     }
 
 }
